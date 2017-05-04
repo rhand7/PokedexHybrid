76a439376eea3f894e909c53d6459f7bf8bdfc61
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-05-04 10:23:20 -0600

    Setup ViewControllers and Subclasses

---
+++ PokedexHybrid/PokedexHybrid/PokemonSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonSearchViewController.swift
+//  PokedexHybrid
+//
+//  Created by handje on 5/4/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+   
+}
+
+ 
