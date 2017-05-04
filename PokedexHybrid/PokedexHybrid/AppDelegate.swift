5c84d91179388830a45f398e6f7c1e661c0d37dd
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-05-04 10:16:31 -0600

    Project cleanup

---
+++ PokedexHybrid/PokedexHybrid/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  PokedexHybrid
+//
+//  Created by handje on 5/4/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
